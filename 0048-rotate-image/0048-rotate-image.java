42d708657c00898a831ed846aab68cc93b5af777
Gulam ZAFAR ALI <alizafar6232@gmail.com>
2026-08-01 22:09:19 +0530

    Time: 1 ms (2.26%), Space: 43.9 MB (35.04%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,29 +1,21 @@
+import java.util.*;
+
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
+        Stack<Integer> stack = new Stack<>();
 
-        // traversing the upper triangle 
-        for(int i = 0;i<n;i++){
-            for(int j=i;j<n;j++){
-            // swapping the elements
-            int temp = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i] = temp;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                stack.push(matrix[i][j]);
             }
         }
 
-        // reversing rows
-        for(int i =0;i<n;i++){
-            int left =0;
-            int right = n-1;
+        for (int i = 0; i < n; i++) {
+            for (int j = matrix[0].length - 1; j >= 0; j--) {
+                int val = stack.pop();
 
-            while(left<right){
-                int temp = matrix[i][left];
-                matrix[i][left] = matrix[i][right];
-                matrix[i][right] = temp;
-
-                left++;
-                right--;
+                matrix[j][i] = val;
             }
         }
     }
