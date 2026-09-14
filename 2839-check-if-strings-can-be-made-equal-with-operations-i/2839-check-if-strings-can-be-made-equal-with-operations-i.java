ea4a159fc351b1d803e3180d5a75454cfd18bb24
Gulam ZAFAR ALI <alizafar6232@gmail.com>
2026-09-14 21:22:38 +0530

    Time: 1 ms (99.92%), Space: 44 MB (85.85%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,51 +1,22 @@
 class Solution {
-
     public boolean canBeEqual(String s1, String s2) {
+        
+        // group position 0 and 1
+        boolean group1 = samePair(
+            s1.charAt(0), s1.charAt(2),
+            s2.charAt(0), s2.charAt(2)
+        );
 
-        // Test 1: No swap
-        if (s1.equals(s2))
-            return true;
-
-        // Test 2: Swap positions (0,2)
-        if (swap0_2(s1).equals(s2))
-            return true;
-
-        // Test 3: Swap positions (1,3)
-        if (swap1_3(s1).equals(s2))
-            return true;
-
-        // Test 4: Swap both (0,2) and (1,3)
-        String test = swap0_2(s1);
-
-        if (swap1_3(test).equals(s2))
-            return true;
-
-        return false;
+        // gorup position 1 and 3
+        boolean group2 = samePair(
+            s1.charAt(1), s1.charAt(3),
+            s2.charAt(1), s2.charAt(3)
+        );
+        return group1 & group2;
     }
+    private boolean samePair(char ch1,char ch2,char ch3,char ch4){
 
-
-    // Swap index 0 and index 2
-    private String swap0_2(String s) {
-
-        char[] arr = s.toCharArray();
-
-        char temp = arr[0];
-        arr[0] = arr[2];
-        arr[2] = temp;
-
-        return new String(arr);
-    }
-
-
-    // Swap index 1 and index 3
-    private String swap1_3(String s) {
-
-        char[] arr = s.toCharArray();
-
-        char temp = arr[1];
-        arr[1] = arr[3];
-        arr[3] = temp;
-
-        return new String(arr);
+        return (ch1 == ch3 && ch2 == ch4) ||
+        (ch1 == ch4 && ch2 == ch3);
     }
 }
